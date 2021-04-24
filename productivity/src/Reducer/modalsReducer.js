001934c5a1c06aa1ea5b468ee027ddfb7a3d381f
Ruhee S <ruhee21@github.com>
2021-04-24 17:40:42 -0500

    Added toggle, edit, delete to task list

--- productivity/src/Reducer/modalsReducer.js
+++ productivity/src/Reducer/modalsReducer.js
@@ -3,20 +3,6 @@ import {
 } from "../Constant/actionTypes";
 
 
-<<<<<<< HEAD
-const setModal = (state, action) => ({
-    ...state,
-    modalType: action.modalType
-});
-
-
-export const modalsReducer = (state = defaultState, action) => {
-    const reducerMapper = {
-        [SET_MODAL]: setModal
-    }[action.type];
-
-    return reducerMapper ? reducerMapper(state, action) : state;
-=======
 export const modalsReducer = (state = {modalType: undefined}, action) => {
     switch(action.type){
         case SET_MODAL:
@@ -27,5 +13,4 @@ export const modalsReducer = (state = {modalType: undefined}, action) => {
         default:
             return state
     }
->>>>>>> 410c5b897f4f0fae9625943882aa52ffa3c572c9
 }
