f5a7c7ccc4e9e4288cda3c426325870034a682a3
Vu Ngoc Phuong Dinh <55153046+irisdinh@users.noreply.github.com>
2021-05-13 14:10:30 -0500

    Update Expense.js

--- productivity/src/Pages/Expense/Expense.js
+++ productivity/src/Pages/Expense/Expense.js
@@ -9,6 +9,7 @@ import { ModalTypes } from '../../Constant/modalTypes';
 import {setModal} from '../../Action/modalsAction';
 import CardItem from './CardItem'
 import AddIcon from '@material-ui/icons/Add';
+import { Money } from "../Dashboard/ExpenseModule/Money";
 
 const Expense = ()=> {
     const filterDate = ['All', 'Today', '3 Recent Days', 'One Week']
@@ -54,96 +55,98 @@ const Expense = ()=> {
     
     return (
         <div>
-            <div>
-                <Row> 
-                    <Col>
-                        <Card>
-                            <Card.Body>
-                                <h5> Total: {-spending + earning}</h5>
-                                <p> Spending: {spending}</p>
-                                <p> Earning: {earning} </p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <p className='intro'>Filter by Date</p>
-                        <Form.Control as='select' id='filter'
-                            defaultValue = {infoFilter.date}
-                            onChange={(e) => dispatch({
-                                type: 'SETF',
-                                key: 'date',
-                                value: e.target.value,
-                                
-                            })}>
-                            {filterDate.map((id) => 
-                                <option>
-                                    {id}
-                                </option>
-                            )}
-                        </Form.Control>    
-                    </Col>
-                    <Col>
-                        <p className='intro'>Filter by Type</p>
-                        <Form.Control as='select' id='filter'
-                            defaultValue = {infoFilter.type}
-                            onChange={(e) => dispatch({
-                                type: 'SETF',
-                                key: 'type',
-                                value: e.target.value,
-                                
-                            })}>
-                            {filterType.map((id) => 
-                                <option>
-                                    {id}
-                                </option>
-                            )}
-                        </Form.Control>
-                    </Col>
-                </Row>   
-            </div>
-            <div>
-                <Card className='card'>
-                    <Card.Header style={{fontSize:'25px', backgroundColor:'#fcf4a3'}}> 
-                        Finance
-                        <Button className='addButton' variant="outline-primary" onClick={() => dispatch(setModal(ModalTypes.EXPENSE))}
-                            style={{float:'right'}}> 
-                            <AddIcon/>Add New Expense 
-                        </Button> 
-                    </Card.Header>
-                    <Card.Body>
-                        <Table>
-                            <thead>
-                                <tr style={{padding:'1px', fontSize:'20px', margin:'1px'}}>
-                                    <th>Date</th>
-                                    <th>Name</th>
-                                    <th>Amount</th>
-                                    <th>Type</th>
-                                    <th>Edit</th>
-                                    <th>Delete</th>
-                                </tr>
-                            </thead>
-                                {infoFilter.filterList.length > 0 ?
-                                    <tbody>
-                                        {infoFilter.filterList.map((expenseItem) =>
-                                            <CardItem item={expenseItem}/>
-                                        )}
-                                    </tbody>
-                                        :
-                                    <div>
-                                        <Lottie
-                                            animationData={animationData}
-                                            loop
-                                            play
-                                            style={{ width: '30rem', height: '30rem' }}
-                                        />
-                                        <p style = {{fontSize:'20px', fontStyle:'bold', marginLeft:'2em'}}> Your Expense List is empty. Add New Expense to start </p>
-                                     
-                                    </div>    
-                                    }
-                        </Table>
-                    </Card.Body>
-                </Card>  
-            </div>       
+            <Row>
+                <Col sm={3}> 
+                    <Card>
+                        <Card.Header style={{fontSize:'25px', fontWeight:'bold'}}>
+                            Expense Information
+                        </Card.Header>
+                        <Card.Body>
+                            <Money label={'Earning'} amount={earning}/>
+                            <Money label={'Spending'} amount={spending}/>
+                            <div className='divider p-0 my-2'/>
+                            <Money
+                                variant={earning - spending < 0 ? 'negative' : 'positive'}
+                                label={'Total'}
+                                amount={earning - spending}
+                            />
+                        </Card.Body>
+                    </Card>
+                    <p className='intro'>Filter by Date</p>
+                    <Form.Control as='select' id='filter'
+                        defaultValue = {infoFilter.date}
+                        onChange={(e) => dispatch({
+                            type: 'SETF',
+                            key: 'date',
+                            value: e.target.value,
+                            
+                        })}>
+                        {filterDate.map((id) => 
+                            <option>
+                                {id}
+                            </option>
+                        )}
+                    </Form.Control>
+                    <p className='intro'>Filter by Type</p>
+                    <Form.Control as='select' id='filter'
+                        defaultValue = {infoFilter.type}
+                        onChange={(e) => dispatch({
+                            type: 'SETF',
+                            key: 'type',
+                            value: e.target.value,
+                            
+                        })}>
+                        {filterType.map((id) => 
+                            <option>
+                                {id}
+                            </option>
+                        )}
+                    </Form.Control>
+                </Col>
+                <Col sm={9}> 
+                    <Card className='card'>
+                        <Card.Header style={{fontSize:'25px', backgroundColor:'#fcf4a3', fontWeight:'bold'}}> 
+                            Finance
+                            <Button className='addButton' variant="outline-primary" onClick={() => dispatch(setModal(ModalTypes.EXPENSE))}
+                                style={{float:'right'}}> 
+                                <AddIcon/>Add New Expense 
+                            </Button> 
+                        </Card.Header>
+                        <Card.Body>
+                            <Table>
+                                <thead>
+                                    <tr style={{padding:'1px', fontSize:'20px', margin:'1px'}}>
+                                        <th>Date</th>
+                                        <th>Name</th>
+                                        <th>Amount</th>
+                                        <th>Type</th>
+                                        <th>Edit</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                </thead>
+                                    {infoFilter.filterList.length > 0 ?
+                                        <tbody>
+                                            {infoFilter.filterList.map((expenseItem) =>
+                                                <CardItem item={expenseItem}/>
+                                            )}
+                                        </tbody>
+                                            :
+                                        <div>
+                                            <Lottie
+                                                animationData={animationData}
+                                                loop
+                                                play
+                                                style={{ width: '30rem', height: '30rem' }}
+                                            />
+                                            <p style = {{fontSize:'20px', fontStyle:'bold', marginLeft:'2em'}}> Your Expense List is empty. Add New Expense to start </p>
+                                        
+                                        </div>    
+                                        }
+                            </Table>
+                        </Card.Body>
+                    </Card>  
+                </Col>
+            </Row>
         </div>
     )
 }
