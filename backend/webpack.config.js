34dcaa3a6e0bc13da62607e8946688f18ea6516a
DucPham17 <phamminhduckl@gmail.com>
2021-05-25 02:43:50 -0500

    change backend

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -6,5 +6,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'api.bundle.js'
   },
+  externals: {'@google-cloud/storage': 'commonjs @google-cloud/storage' },
   target: 'node'
 };
